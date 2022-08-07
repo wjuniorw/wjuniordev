c37056276a0a13bcdafb46f055410e831afd4c36
Wilson Junior <wilson.junior90@gmail.com>
2022-08-06 21:15:37 -0300

    change link font prop to camelCase

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,9 +38,16 @@ class CustomDoc extends Document {
     return (
       <Html lang='pt-BR'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='*'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap'
+            rel='stylesheet'
+          />
         </Head>
         <body>
           <Main />
